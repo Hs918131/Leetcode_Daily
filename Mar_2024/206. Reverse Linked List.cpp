df6a905f96f78629ce8aedab273fedaa6ef0fcd2
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2024-03-22 00:08:12 +0530

    Update 206. Reverse Linked List.cpp

--- Mar_2024/206. Reverse Linked List.cpp
+++ Mar_2024/206. Reverse Linked List.cpp
@@ -1,50 +1,24 @@
-//T.C : O(m+n)
-//T.C : O(1) (Ignoring recursion stack space)
-class Solution
-{
-    public:
-    
-    struct Node* mergeTwoLists(Node *list1,Node *list2) {
-        if(list1 == NULL) {
-            return list2;
-        }
-
-        if(list2 == NULL) {
-            return list1;
-        }
-
-        struct Node* result;
-        if(list1->data < list2->data) {
-            result = list1;
-            result->next = mergeTwoLists(list1->next, list2);
-        } else {
-            result = list2;
-            result->next = mergeTwoLists(list1, list2->next);
-        }
-
-        return result;
-    }
-    
-    struct Node* reverseList(struct Node* head) {
-        if(head == NULL || head->next == NULL) {
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head -> next == NULL){
             return head;
         }
+        ListNode* last = reverseList(head -> next);
 
-        struct Node* last = reverseList(head->next); //last = future head
-
-        head->next->next = head;
-        head->next = NULL;
+        head -> next -> next = head;
+        head -> next = NULL;
 
         return last;
     }
-    
-    struct Node * mergeResult(Node *node1,Node *node2) {
-        struct Node* result = mergeTwoLists(node1, node2);
-        
-        
-        result = reverseList(result);
-        
-        return result;
-        
-    }  
 };
