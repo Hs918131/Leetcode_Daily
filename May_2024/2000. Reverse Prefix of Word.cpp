12b6d7912519a442ffcce1165d0f5b746a8cd7e3
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2024-05-01 23:05:59 +0530

    Create 2000. Reverse Prefix of Word.cpp

---
+++ May_2024/2000. Reverse Prefix of Word.cpp
@@ -0,0 +1,32 @@
+//Using simple loop and swap
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int i = 0;
+        int j = word.find(ch);
+        
+        while(i < j) {
+            swap(word[i], word[j]);
+            i++;
+            j--;
+        }
+        
+        return word;
+        
+    }
+};
+
+
+//Using Library Functions
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        reverse(begin(word), word.begin()+word.find(ch)+1);
+        return word;
+        
+    }
+};
