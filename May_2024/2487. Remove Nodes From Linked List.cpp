0cfaf72fbdc695588de3bd6c1b3c08cb4c697467
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2024-05-07 19:42:09 +0530

    Create 2487. Remove Nodes From Linked List.cpp

---
+++ May_2024/2487. Remove Nodes From Linked List.cpp
@@ -0,0 +1,105 @@
+//Approach-1 (Using stack)
+//T.C  : O(n)
+//S.C  : O(n)
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        stack<ListNode*> st;
+        ListNode* current = head;
+
+        while (current != nullptr) {
+            st.push(current);
+            current = current->next;
+        }
+
+        current = st.top();
+        st.pop();
+        int maximum = current->val;
+        ListNode* resultHead = new ListNode(maximum);
+
+        while (!st.empty()) {
+            current = st.top();
+            st.pop();
+            if (current->val < maximum) {
+                continue;
+            }
+            else {
+                ListNode* newNode = new ListNode(current->val);
+                newNode->next = resultHead;
+                resultHead = newNode;
+                maximum = current->val;
+            }
+        }
+
+        return resultHead;
+    }
+};
+
+
+
+//Approach-2 (Using Recursion - instead of stack, use system's stack)
+//T.C : O(n)
+//S.C : O(n) system stack
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+
+        ListNode* nextNode = removeNodes(head->next);
+
+        if (head->val < nextNode->val) {
+            delete head;
+            return nextNode;
+        }
+
+        head->next = nextNode;
+        return head;
+    }
+};
+
+
+//Approach-3 (Using reverse)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+private:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL) {
+            return head;
+        }
+        
+        ListNode* last = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        
+        return last;
+    }
+public:
+    ListNode* removeNodes(ListNode* head) {
+        head = reverseList(head);
+
+        int maximum = 0;
+        ListNode* prev = NULL;
+        ListNode* current = head;
+
+        while (current != NULL) {
+            maximum = max(maximum, current->val);
+
+            if (current->val < maximum) {
+                prev->next = current->next;
+                ListNode* deleted = current;
+                current = current->next;
+                delete(deleted);
+            }
+
+            else {
+                prev = current;
+                current = current->next;
+            }
+        }
+        
+        return reverseList(head);
+    }
+};
