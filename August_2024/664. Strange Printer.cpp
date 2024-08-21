bee0c12047b3c3d909e410cb64aeef13da296496
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2024-08-21 23:03:16 +0530

    Create 664. Strange Printer.cpp

---
+++ August_2024/664. Strange Printer.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void shorten(string& s, int& n){
+        int sz=s.size();
+        n=1;
+        char cur;
+        for(int l=0, r=1; r<sz; r++){
+            cur=s[r];
+            while(l<r && s[l]==cur) l++;
+            if (r<sz && s[l]!=cur){
+                s[n++]=s[r];
+                l=r;
+            }
+        }
+        s.resize(n);
+    }
+
+    int dp[101][101];
+    int f(int i, int j, string& s){
+        if (dp[i][j]!=-1) return dp[i][j];
+        if (i==j) return dp[i][j]=1;
+        if (s[i]==s[j]) return f(i, j-1, s);
+        int ans=f(i, j-1, s)+1;
+        for(int k=i+1; k<j-1; k++){
+            if (s[k]==s[j])
+                ans=min(ans, f(i, k-1, s)+f(k, j-1, s));
+        }
+        return dp[i][j]=ans;
+    }
+    int strangePrinter(string& s) {
+        int n;
+        shorten(s, n);
+        memset(dp, -1, sizeof(dp));
+        return f(0, n-1, s);
+    }
+};
